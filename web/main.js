33fd9bc4d2d21269eba41ac24353b91f7bb4969d
Thomas Forde <thomas.forde@factset.com>
2021-12-23 01:17:27 +0000

    updating the UI to use Bulma

--- web/main.js
+++ web/main.js
@@ -14,6 +14,7 @@ window.addEventListener("DOMContentLoaded", () => {
 function onServerOpen() {
     const params = new URLSearchParams(window.location.search);
     if (params.has("room")) room.join(params.get('room'))
+    if (params.has("host")) updateHostStatus(true)
     page.room.input.addEventListener('input', onRoomCode)
     page.room.button.addEventListener('click', onRoomButton)
     page.name.input.addEventListener('input', onNameUpdate)
@@ -28,23 +29,16 @@ function onServerMessage({ data }) {
     const event = JSON.parse(data)
     switch (event.type) {
         case 'room created':
-            isHost = true
             updateRoomCode(event.data)
-            updateRoomButton()
-            page.quizControls.buttons.style.display = 'grid'
-            page.quizControls.title.style.display = 'unset'
-            page.title.textContent += ' (Host)'
-            page.name.label.hidden = false
+            updateHostStatus(true)
             break;
         case 'room joined':
-            isHost = false
             updateRoomCode(event.data)
+            updateHostStatus(false)
             page.room.button.remove()
             page.room.input.disabled = true
-            page.room.label.hidden = false
-            page.name.label.hidden = false
             page.name.input.value = event.player
-            addPlayer(event.player, false)
+            players.push(page.createPlayer(event.player, room, false))
             break;
         case 'name updated':
             page.name.input.value = event.data
@@ -69,12 +63,12 @@ function updatePlayerList(playerList) {
     }, [])
     const playerNames = players.map(player => player.name)
     const playersToAdd = playerList.filter(playerName => !playerNames.includes(playerName))
-    playersToAdd.forEach(playerName => addPlayer(playerName))
+    playersToAdd.forEach(playerName => players.push(page.createPlayer(playerName, room, true)))
 }
 
 function updatePlayerBox(playerName) {
     players.forEach(player => player.remove())
-    addPlayer(playerName, false)
+    players.push(page.createPlayer(playerName, room, false))
 }
 
 function updateRoomCode(code) {
@@ -91,46 +85,22 @@ function onRoomButton() {
     else room.create()
 }
 
-function updateRoomButton() {
-    page.room.input.disabled = true
-    page.room.label.hidden = false
-    page.connect.tag.id = 'room-button'
-    page.connect.tag.href = `?room=${roomCode}`
-    page.connect.tag.target = '_blank'
-    page.connect.button.textContent = 'Connect to Room'
-    page.connect.tag.appendChild(page.connect.button)
-    page.room.button.replaceWith(page.connect.tag)
-}
-
 function onNameUpdate(event) {
     if (!roomCode) return
     room.updateName(event.target.value)
 }
 
-function addPlayer(playerName, readOnly = true) {
-    const player = {
-        div: document.createElement('div'),
-        label: document.createElement('label'),
-        input: document.createElement('input'),
-        correct: document.createElement('button'),
-        name: playerName,
-        remove() { this.div.remove() }
+function updateHostStatus(newIsHost) {
+    isHost = newIsHost
+    if (isHost) {
+        page.room.input.disabled = true
+        page.name.input.value = 'HOST'
+        page.name.input.disabled = true
+        page.room.buttonControl.hidden = true
+        page.room.connectControl.hidden = false
+        page.room.connectTag.href = `?room=${roomCode}`
+        page.quizControls.panel.hidden = false
     }
-    player.div.className = 'player-div'
-    player.div.id = `${playerName}-div`
-    player.label.textContent = `${playerName}: `
-    player.input.id = playerName
-    player.input.placeholder = "What's the answer?"
-    player.input.disabled = readOnly
-    player.input.addEventListener('input', ({ target }) => room.answer(target.value))
-    player.correct.id = `${playerName}-answer`
-    player.correct.textContent = 'Correct'
-    player.correct.addEventListener('click', () => room.answerResponse('correct', player.input.value, playerName))
-    player.div.appendChild(player.label)
-    player.div.appendChild(player.input)
-    if (isHost) player.div.appendChild(player.correct)
-    page.quizInputs.div.appendChild(player.div)
-    players.push(player)
 }
 
 function getWebSocketServer() {
--- web/page-elements.js
+++ web/page-elements.js
@@ -3,6 +3,9 @@ export const title = document.querySelector('#title')
 export const room = {
     input: document.querySelector('#room-code'),
     button: document.querySelector('#room-button'),
+    buttonControl: document.querySelector('#room-button-control'),
+    connectControl: document.querySelector('#connect-room-control'),
+    connectTag: document.querySelector('#connect-room-tag'),
     label: document.querySelector('#room-label')
 }
 
@@ -11,27 +14,72 @@ export const name = {
     label: document.querySelector('#name-label')
 }
 
-export const connect = {
-    tag: document.createElement('a'),
-    button: document.createElement('button'),
-}
-
 export const quizControls = {
-    title: document.querySelector('#control-title'),
+    panel: document.querySelector('#quiz-control-panel'),
+    title: document.querySelector('#quiz-title'),
     info: document.querySelector('#quiz-info'),
     start: document.querySelector('#quiz-start'),
     end: document.querySelector('#quiz-end'),
     pause: document.querySelector('#quiz-pause'),
     unpause: document.querySelector('#quiz-unpause'),
-    buttons: document.querySelector('#control-buttons'),
 }
 
-export const quizInputs = {
-    title: document.querySelector('#input-title'),
-    div: document.querySelector('#inputs')
+export const playerInputs = {
+    title: document.querySelector('#inputs-title'),
+    div: document.querySelector('#inputs-container')
 }
 
 export const players = {
     div: document.querySelectorAll('.player-div'),
     input: document.querySelectorAll('.player-div input'),
+}
+
+export const createPlayer = (playerName, room, hostControls = false) => {
+    const player = {
+        field: document.createElement('div'),
+        fieldLabel: document.createElement('div'),
+        label: document.createElement('label'),
+        fieldBody: document.createElement('div'),
+        fieldBodyField: document.createElement('div'),
+        inputControl: document.createElement('div'),
+        input: document.createElement('input'),
+        admin: {
+            correctControl: document.createElement('div'),
+            correctButton: document.createElement('button')
+        },
+        name: playerName,
+        remove() { this.field.remove() }
+    }
+    player.field.className = 'field is-horizontal'
+    player.fieldLabel.className = 'field-label is-normal'
+    player.label.className = 'label'
+    player.label.textContent = playerName
+    player.fieldBody.className = 'field-body'
+    player.fieldBodyField.className = 'field'
+    player.inputControl.className = 'control'
+    player.input.type = 'text'
+    player.input.className = 'input'
+    player.input.id = playerName
+    player.input.placeholder = hostControls ? "Awaiting player" : "What's the answer?"
+    player.input.addEventListener('input', ({ target }) => room.answer(target.value))
+    player.field.appendChild(player.fieldLabel)
+    player.fieldLabel.appendChild(player.label)
+    player.field.appendChild(player.fieldBody)
+    player.fieldBody.appendChild(player.fieldBodyField)
+    player.fieldBodyField.appendChild(player.inputControl)
+    player.inputControl.appendChild(player.input)
+    if (hostControls) {
+        player.fieldBodyField.className += ' has-addons'
+        player.inputControl.className += ' is-expanded'
+        player.input.disabled = true
+        player.admin.correctControl.className = 'control'
+        player.admin.correctButton.className = 'button is-success'
+        player.admin.correctButton.innerText = 'Correct'
+        player.admin.correctButton.id = `${playerName}-answer`
+        player.admin.correctButton.addEventListener('click', () => room.answerResponse('correct', player.input.value, playerName))
+        player.fieldBodyField.appendChild(player.admin.correctControl)
+        player.admin.correctControl.appendChild(player.admin.correctButton)
+    }
+    playerInputs.div.appendChild(player.field)
+    return player
 }
